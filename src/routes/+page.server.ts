c4300139ca83edfd59f497876157adb827b19832
Ahmed <ahmed.lahrizi05@gmail.com>
2025-08-24 10:28:01 +0100

    rollbacked

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,23 +1,21 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 // import { eq } from 'drizzle-orm';
 import { usersTable } from '../db/schema';
-import { Pool } from "pg";
+import { Pool } from 'pg';
 import 'dotenv/config';
 
-
 const pool = new Pool({
-//    connectionString: process.env.HYPERDRIVE.connectionString!,
-    connectionString: process.env.DEV_DB!,
+	//    connectionString: process.env.HYPERDRIVE.connectionString!,
+	connectionString: process.env.DEV_DB!
 });
 const db = drizzle({ client: pool });
 
-
 export async function load() {
-    const users = await db.select().from(usersTable);
-    console.log(process.env!);
-    console.log('Getting all users from the database: ', users);
-    /*
-     */
-    console.log(users)
-    return users[0];
+	const users = await db.select().from(usersTable);
+	console.log(process.env!);
+	console.log('Getting all users from the database: ', users);
+	/*
+	 */
+	console.log(users);
+	return users[0];
 }
