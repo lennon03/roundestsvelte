dadbf699927808953808a9685b9b0be8a012ff31
Ahmed <ahmed.lahrizi05@gmail.com>
2025-08-24 10:49:29 +0100

    fixed hyperdrive yet again

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,17 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-// import { eq } from 'drizzle-orm';
 import { usersTable } from '../db/schema';
 import { Pool } from 'pg';
-import 'dotenv/config';
 
-const pool = new Pool({
-	//    connectionString: process.env.HYPERDRIVE.connectionString!,
-	connectionString: process.env.DEV_DB!
-});
-const db = drizzle({ client: pool });
+export async function load({ request, platform }) {
+	const pool = new Pool({
+		connectionString: platform!.env?.HYPERDRIVE?.connectionString
+	});
 
-export async function load() {
+	const db = drizzle({ client: pool });
+
 	const users = await db.select().from(usersTable);
-	console.log(process.env!);
+	console.log(platform!.env?.HYPERDRIVE?.connectionString);
 	console.log('Getting all users from the database: ', users);
-	/*
-	 */
 	console.log(users);
 	return users[0];
 }
